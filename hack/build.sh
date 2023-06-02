862f992ed5998bb06f194069eefe126d8c782ea4
Engin Diri <engin.diri@ediri.de>
2023-06-02 23:54:19 +0200

    feat: initial commit

--- hack/build.sh
+++ hack/build.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
-
-echo "Current working directory is $(pwd)"
-echo "PATH is $PATH"
-echo "CARGO_HOME is $CARGO_HOME"
+#!/usr/bin/env bash
+set -e
 
 PROVIDER_ROOT=$(git rev-parse --show-toplevel)
 COMMIT_HASH=$(git rev-parse --short HEAD 2>/dev/null)
 DATE=$(date "+%Y-%m-%d")
 BUILD_PLATFORM=$(uname -a | awk '{print tolower($1);}')
+
+echo "Current working directory is $(pwd)"
 
 if [[ "$(pwd)" != "${PROVIDER_ROOT}" ]]; then
   echo "you are not in the root of the repo" 1>&2
@@ -15,39 +14,5 @@ if [[ "$(pwd)" != "${PROVIDER_ROOT}" ]]; then
   exit 1
 fi
 
-RUST_BUILD_CMD="cargo build"
-RUST_BUILD_FLAGS="--release"
-
-if [[ -z "${PROVIDER_BUILD_PLATFORMS}" ]]; then
-    PROVIDER_BUILD_PLATFORMS="unknown-linux-gnu pc-windows-msvc" # pc-windows-msvc apple-darwin"
-fi
-
-if [[ -z "${PROVIDER_BUILD_ARCHS}" ]]; then
-    PROVIDER_BUILD_ARCHS="x86_64 aarch64"
-fi
-
-# Create the release directory
-mkdir -p "${PROVIDER_ROOT}/release"
-
-cargo install cross --git https://github.com/cross-rs/cross
-
-for OS in ${PROVIDER_BUILD_PLATFORMS[@]}; do
-  for ARCH in ${PROVIDER_BUILD_ARCHS[@]}; do
-    NAME="devpod-provider-hetzner-${OS}-${ARCH}"
-    if [[ "${OS}" == "pc-windows-msvc" ]]; then
-      NAME="${NAME}.exe"
-    fi
-
-    if [[ "${ARCH}" == "aarch64" && "${OS}" == "pc-windows-msvc" ]]; then
-        echo "Building for ${OS}/${ARCH} not supported."
-        continue
-    fi
-
-    echo "Building for ${OS}/${ARCH}"
-    cross build --target ${ARCH}-${OS} --release
-    #rustup target add ${ARCH}-${OS}
-    #RUSTFLAGS="--target ${ARCH}-${OS}" ${RUST_BUILD_CMD} ${RUST_BUILD_FLAGS}
-    cp target/${ARCH}-${OS}/release/devpod-provider-hetzner "${PROVIDER_ROOT}/release/${NAME}"
-    shasum -a 256 "${PROVIDER_ROOT}/release/${NAME}" | cut -d ' ' -f 1 > "${PROVIDER_ROOT}/release/${NAME}".sha256
-  done
-done
+# generate provider.yaml
+go run -mod vendor "${PROVIDER_ROOT}/hack/provider/main.go" ${RELEASE_VERSION} > "${PROVIDER_ROOT}/release/provider.yaml"
